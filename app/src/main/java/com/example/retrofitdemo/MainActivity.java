9bfa1f151ccc3e93e5c5b809227ff3d19506cf2e
Amit <cscience.amit@gmail.com>
2020-07-22 17:36:07 +0530

    generic

--- app/src/main/java/com/example/retrofitdemo/MainActivity.java
+++ app/src/main/java/com/example/retrofitdemo/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.retrofitdemo;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,18 +14,23 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity implements ApiResponseHandler {
 
+    private TextView txt_title, txt_email;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final TextView txt_title = findViewById(R.id.txt_title);
-        final TextView txt_email = findViewById(R.id.txt_email);
+        txt_title = findViewById(R.id.txt_title);
+        txt_email = findViewById(R.id.txt_email);
         JsonClassAPi classAPi = RetrofitService.api;
 
         Call<List<PostModel>> call1 = classAPi.getTitle();
         Call<List<CommentsModel>> call2 = classAPi.getEmail();
 
-        call1.enqueue(new Callback<List<PostModel>>() {
+        new ResponseJavaClass().executeApi(this, call1);
+        new ResponseJavaClass().executeApi(this, call2);
+
+ /*       call1.enqueue(new Callback<List<PostModel>>() {
             @Override
             public void onResponse(Call<List<PostModel>> call, Response<List<PostModel>> response) {
                 List<PostModel> model = response.body();
@@ -39,7 +45,8 @@ public class MainActivity extends AppCompatActivity implements ApiResponseHandle
             }
         });
 
-        call2.enqueue(new Callback<List<CommentsModel>>() {
+
+          call2.enqueue(new Callback<List<CommentsModel>>() {
             @Override
             public void onResponse(Call<List<CommentsModel>> call, Response<List<CommentsModel>> response) {
                 List<CommentsModel> model = response.body();
@@ -51,17 +58,18 @@ public class MainActivity extends AppCompatActivity implements ApiResponseHandle
             public void onFailure(Call<List<CommentsModel>> call, Throwable t) {
 
             }
-        });
+        });*/
 
     }
 
     @Override
-    public void onResponse(Object o) {
-
+    public void onResponse(Object response) {
+        List<PostModel> model = (List<PostModel>) response;
+        txt_title.setText(model.get(0).title);
     }
 
     @Override
-    public void onFailure(Object o) {
+    public void onFailure(Object error) {
 
     }
 }
--- app/src/main/java/com/example/retrofitdemo/ResponseJavaClass.java
+++ app/src/main/java/com/example/retrofitdemo/ResponseJavaClass.java
@@ -5,15 +5,11 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class ResponseJavaClass<T> {
-    void enqueueCallBack(final ApiResponseHandler data, Call<T> call) {
+    void executeApi(final ApiResponseHandler data, Call<T> call) {
         call.enqueue(new Callback<T>() {
             @Override
             public void onResponse(Call<T> call, Response<T> response) {
                 data.onResponse(response.body());
-              /*  T model = response.body();
-                if (model != null && model.size() > 0)
-                    txt_title.setText(model.get(0).title);*/
-
             }
 
             @Override
